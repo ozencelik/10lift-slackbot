a5abacd638f6981dec3d6da35856cb98eac64e82
Ozenc Celik <o.celik@tr.emakina.com>
2021-05-12 09:18:11 +0300

    - Try to connect db - Db variables updated as the correct ones.

--- src/properResponse.js
+++ src/properResponse.js
@@ -1,4 +1,6 @@
-const Pool = require("pg").Pool;
+const { Pool, Client } = require('pg')
+var pool;
+var client;
 
 const properResponse = (command, user_id, user_name, time) => {
     switch (command) {
@@ -16,7 +18,7 @@ const properResponse = (command, user_id, user_name, time) => {
 const getLeaderboard = () => {
     return '1. ozenc.celik 50\n2. marry.jane 25\n3. john.doe 20';
 
-    
+
     pool.query(
         "SELECT * FROM user_activity",
         [],
@@ -32,23 +34,47 @@ const getLeaderboard = () => {
 
 
 const insertNewActivity = (command, user_id, user_name, time) => {
-
+    if (!client) {
+        console.log('if !pool girdi');
+        buildPool();
+    }
+ 
+    
+    console.log('1');
+    client.connect()
+    console.log('2');
+    client.query('SELECT NOW()', (err, res) => {
+        console.log(err, res)
+        client.end()
+    })
+    /*
+        pool.query(
+            "INSERT INTO user_activity (user_id, activity_id, point, created_on) VALUES ($1, $2, $3, $4)",
+            [1, 1, 18, new Date()],
+            (error, results) => {
+                if (error) {
+                    throw error;
+                }
+                console.log('Sanırım ekledi !!!');
+            }
+        );
+    */
     return 'Unauthorized';
 }
 
 
-const runQuery = (query) => {
-
-    client
-    .query(query)
-    .then(res => {
-        console.log('Table is successfully created');
-    })
-    .catch(err => {
-        console.error(err);
-    })
-    .finally(() => {
-        client.end();
+const buildPool = () => {
+    // Create a connection to the Heroku Postgresql Db for each time.
+    client = new Client({
+        user: process.env.USER,
+        host: process.env.HOST,
+        database: process.env.DATABASE,
+        password: process.env.PASSWORD,
+        port: process.env.DB_PORT,
+        ssl: {
+            require: true, // This will help you. But you will see nwe error
+            rejectUnauthorized: false // This line will fix new error
+        }
     });
 }
 
